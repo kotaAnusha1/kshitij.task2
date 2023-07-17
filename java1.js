eaddb39aee428ebe60cf42384baba49351114c60
kotaAnusha1 <138881143+kotaAnusha1@users.noreply.github.com>
2023-07-17 21:39:27 +0530

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,79 @@
+const N = 4;
+
+function shuffleArray(array) {
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+  
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+}
+
+let dataBase = [];
+
+let array = [];
+
+for(let i=0;i<N*N;i++) {
+    array.push(i);
+}
+
+
+function getInvCount(arr) {
+    let inv_count = 0;
+    for (let i = 0; i < N * N - 1; i++){   
+        if(arr[i]!= 0){
+            for (let j = i + 1; j < N * N; j++){
+            
+                if (arr[j] && arr[i] && arr[i] > arr[j]){
+                    inv_count++;
+                }
+            }
+        }
+    }
+    return inv_count;
+}
+
+function findXPosition(puzzle){
+
+for (let i = N - 1; i >= 0; i--){
+    for (let j = N - 1; j >= 0; j--){
+    if (puzzle[i][j] == 0)
+        return N - i;
+    }
+}
+}
+
+
+function isSolvable(puzzle){
+
+let invCount = getInvCount(puzzle);
+
+    if (N & 1)
+        return !(invCount & 1);
+
+    else {   
+        let pos = findXPosition(puzzle);
+        if (pos & 1)
+            return !(invCount & 1);
+        else
+            return invCount & 1;
+    }
+}
+
+let i = 0;
+while (true) {
+    let dataElement = shuffleArray(array);
+    
+    if (isSolvable(dataElement)) {
+        dataBase.push(dataElement);
+        break;
+    }
+}
